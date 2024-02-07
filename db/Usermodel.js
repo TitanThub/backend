8cbc5c94eb7ca76bfd7da3e1413ee06e2011969b
Not-Your-Man <9jacruise007@gmail>
2024-02-07 21:07:48 +0100

    Pushed Changes

--- db/Usermodel.js
+++ db/Usermodel.js
@@ -40,6 +40,10 @@ const userSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  fee: {
+    type: Number,
+    default: 0,
+  },
   role: {
     type: String,
     enum: ["user", "admin"],
